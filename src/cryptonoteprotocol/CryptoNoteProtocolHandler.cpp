94276931c021bcd8e77f2d84c70feaf6490642cd
Yong Jian Ming <jianming1993@gmail.com>
2024-02-10 17:43:05 +0800

    Set proper gcc

--- src/cryptonoteprotocol/CryptoNoteProtocolHandler.cpp
+++ src/cryptonoteprotocol/CryptoNoteProtocolHandler.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2021, The DeroGold Developers
+// Copyright (c) 2018-2024, The DeroGold Developers
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2018-2019, The TurtleCoin Developers
