8ce8c86328a3ac7644a5c19c35c66f0cb5239ca0
Yong Jian Ming <jianming1993@gmail.com>
2024-04-19 00:47:56 +0800

    Update seed node list

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -367,5 +367,7 @@ namespace CryptoNote
 
     const char *const SEED_NODES[] = {
         "80.60.19.222:42069", // sniperviperman
+		"81.169.205.142:42069",
+		"213.199.49.4:42069",
     };
 } // namespace CryptoNote
