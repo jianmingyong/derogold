34959f8a901399dfbfd6b150b91982a8ce825bbd
Yong Jian Ming <jianming1993@gmail.com>
2024-09-10 22:11:12 +0800

    Update

--- docker-build.bat
+++ docker-build.bat
@@ -2,5 +2,5 @@
 
 SET OUTPUT_VERSION=0.7.2.3
 
-docker build -f Dockerfile --platform linux/amd64,linux/arm64 --target release_gcc -t jianmingyong/derogold:latest -t jianmingyong/derogold:latest-gcc -t jianmingyong/derogold:%OUTPUT_VERSION% -t jianmingyong/derogold:%OUTPUT_VERSION%-gcc .
-docker build -f Dockerfile --platform linux/amd64,linux/arm64 --target release_clang -t jianmingyong/derogold:latest-clang -t jianmingyong/derogold:%OUTPUT_VERSION%-clang .
+docker build -f Dockerfile --platform linux/amd64,linux/arm64 --target release_default -t jianmingyong/derogold:latest -t jianmingyong/derogold:latest-gcc -t jianmingyong/derogold:%OUTPUT_VERSION% -t jianmingyong/derogold:%OUTPUT_VERSION%-gcc .
+docker build -f Dockerfile --platform linux/amd64,linux/arm64 --target release_default -t jianmingyong/derogold:latest-clang -t jianmingyong/derogold:%OUTPUT_VERSION%-clang .
